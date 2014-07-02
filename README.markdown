41e2f56216e8f9957ae8b7ec1e219cda1fb03099
Roger Que <nospam+github@alerante.net>
2014-07-02 13:30:32 -0400

    Initial commit

---
+++ README.markdown
@@ -0,0 +1,10 @@
+# mtbrew
+
+[Homebrew](http://brew.sh/) formulae for machine translation software.
+Just run:
+
+    $ brew tap query/mtbrew
+    $ brew install moses  # or whatever you feel like
+
+These formulae are only tested on Mavericks.
+Bug reports are welcome!
---
+++ moses.rb
@@ -0,0 +1,31 @@
+require 'formula'
+
+class Moses < Formula
+  homepage 'http://www.statmt.org/moses/'
+  head 'https://github.com/moses-smt/mosesdecoder.git'
+
+  depends_on 'boost'
+  depends_on 'boost-build' => :build
+
+  def install
+    args = ["--prefix=#{prefix}",
+            "-j#{ENV.make_jobs}",
+            'threading=multi',
+            'install']
+
+    system './bjam', *args
+  end
+
+  test do
+    require 'open3'
+
+    system 'curl', '-O', 'http://www.statmt.org/moses/download/sample-models.tgz'
+    system 'tar', 'zxf', 'sample-models.tgz'
+    cd 'sample-models'
+    Open3.popen3(bin/'moses', '-f', 'phrase-model/moses.ini') do |stdin, stdout, stderr|
+      stdin.write("das ist ein kleines haus\n")
+      stdin.close
+      assert_equal 'this is a small house', stdout.read.strip
+    end
+  end
+end
